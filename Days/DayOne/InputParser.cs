6075c0b5184dedfa83e163c0a8e93dafcf668847
Jed Menard <jmenard@mymail.mines.edu>
2024-12-01 12:33:32 -0800

    AOC-24 Completed day one

---
+++ Days/DayOne/InputParser.cs
@@ -0,0 +1,50 @@
+﻿namespace AdventOfCode2024.Days.DayOne;
+
+public class InputParser
+{
+    public List<int> Left;
+    public List<int> Right;
+
+    public InputParser()
+    {
+        StreamReader inputFile = new StreamReader("F:\\Projects\\AdventOfCode2024\\Days\\DayOne\\input.txt");
+        this.Left = new List<int>();
+        this.Right = new List<int>();
+
+        string? line = inputFile.ReadLine();
+        while (line != null)
+        {
+            string[] inputValues = line.Split("   ");
+
+            if (inputValues.Length != 2)
+            {
+                throw new ApplicationException($"Unexpected number of input values: {inputValues.Length}");
+            }
+
+            Left.Add(int.Parse(inputValues[0]));
+            Right.Add(int.Parse(inputValues[1]));
+
+            line = inputFile.ReadLine();
+        }
+
+        this.Left.Sort();
+        this.Right.Sort();
+    }
+
+    public int GetDistance()
+    {
+        int distance = 0;
+
+        for (int i = 0; i < this.Left.Count; i++)
+        {
+            distance += Math.Abs(this.Left[i] - this.Right[i]);
+        }
+
+        return distance;
+    }
+
+    public int GetSimilarityScore()
+    {
+        return this.Left.Sum(left => left * this.Right.Count(right => right == left));
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using AdventOfCode2024.Days.DayOne;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine(new InputParser().GetSimilarityScore());
+    }
+}
