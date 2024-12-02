94f7631ca3e741a40ce3fc8169a5b1559bfb56ec
Jed Menard <jmenard@mymail.mines.edu>
2024-12-02 13:29:36 -0800

    AOC-24 Added timer

--- Program.cs
+++ Program.cs
@@ -1,9 +1,17 @@
 ﻿using AdventOfCode2024.Days.DayTwo;
+using System.Diagnostics;
 
 internal class Program
 {
     private static void Main(string[] args)
     {
+        Stopwatch sw = new Stopwatch();
+        sw.Start();
+
         Console.WriteLine(new InputParser().ValidReportCount);
+
+        sw.Stop();
+
+        Console.WriteLine($"Processing time (ms): {sw.ElapsedMilliseconds}");
     }
 }
