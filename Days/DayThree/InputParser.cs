4b25b7e2f87d464a6cf799fddb62404b70ed0dd4
Jed Menard <jmenard@mymail.mines.edu>
2024-12-03 11:21:46 -0800

    AOC-24 Completed part one

---
+++ Days/DayThree/InputParser.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.RegularExpressions;
+
+namespace AdventOfCode2024.Days.DayThree;
+
+public class InputParser
+{
+    /// <summary>
+    /// Matches the pattern "mul(###,###)"
+    /// </summary>
+    private Regex multiplyPattern = new Regex(@"mul\([0-9]{1,3},[0-9]{1,3}\)");
+    private List<string> Commands;
+
+    public InputParser()
+    {
+        StreamReader inputFile = new StreamReader("F:\\Projects\\AdventOfCode2024\\Days\\DayThree\\input.txt");
+        this.Commands = new List<string>();
+
+        // Parse the results using regex.
+        string? line = inputFile.ReadLine();
+        while (line != null)
+        {
+            this.Commands.AddRange(this.multiplyPattern.Matches(line)
+                .Select(result => result.Value)
+                .ToList());
+            line = inputFile.ReadLine();
+        }
+
+        return;
+    }
+
+    /// <summary>
+    /// Parses the commands and retuns the result of summing the commands
+    /// </summary>
+    /// <returns></returns>
+    public int GetResult()
+    {
+        // First, strip each command of its "mul" and parenthesis, leaving just the comma-separated values.
+        List<string> csvs = this.Commands.Select(cmd => cmd.Substring(4, cmd.Length - 5)).ToList();
+
+        // Now split them and cast them to ints.
+        List<int[]> values = csvs
+            .Select(csv => csv.Split(",")
+                .Select(str => int.Parse(str))
+                .ToArray()
+            ).ToList();
+
+        // Finally, multiply the numbers and sum them up.
+        return values.Sum(val => val[0] * val[1]);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using AdventOfCode2024.Days.DayTwo;
+﻿using AdventOfCode2024.Days.DayThree;
 using System.Diagnostics;
 
 internal class Program
@@ -8,7 +8,7 @@ internal class Program
         Stopwatch sw = new Stopwatch();
         sw.Start();
 
-        Console.WriteLine(new InputParser().ValidReportCount);
+        Console.WriteLine(new InputParser().GetResult());
 
         sw.Stop();
 
