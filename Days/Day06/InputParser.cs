f6c95c9309954eaae689b9d511e975f495f3d03c
Jed Menard <jmenard@mymail.mines.edu>
2024-12-07 14:30:14 -0800

    AOC-24 Improved performance

--- Days/Day06/InputParser.cs
+++ Days/Day06/InputParser.cs
@@ -4,21 +4,20 @@ namespace AdventOfCode2024.Days.Day06;
 
 public class InputParser
 {
-    private Dictionary<Location, LocationMarker> grid;
+    private Dictionary<Location, char> grid;
     private Location startingLocation = new Location(0,0);
 
     public InputParser()
     {
         StreamReader inputFile = new StreamReader("F:\\Projects\\AdventOfCode2024\\Days\\Day06\\input.txt");
-        this.grid = new Dictionary<Location, LocationMarker>();
+        this.grid = new Dictionary<Location, char>();
 
         int y = 0;
         for (string? line = inputFile.ReadLine(); line != null; line = inputFile.ReadLine(), y++)
         {
             for (int x = 0; x < line.Length; x++)
             {
-                char v = line[x];
-                this.grid[new Location(x, y)] = new LocationMarker(v);
+                this.grid[new Location(x, y)] = line[x];
             }
         }
     }
@@ -26,21 +25,22 @@ public class InputParser
     public int CountTraversedLocations()
     {
         // Traverse the map.
-        this.Traverse(this.grid);
+        this.Traverse(this.grid, out Dictionary<Location, HashSet<DirectionEnum>> traversalPath);
 
         // Return the number of spaces that were traversed.
-        return this.grid.Count(kvp => kvp.Value.HasBeenTraversed());
+        return traversalPath.Count(kvp => !kvp.Value.IsNullOrEmpty());
     }
 
     public int CountPotentialTraversalLoops()
     {
         // Traverse the map once to get the guard's original path.
-        Dictionary<Location, LocationMarker> originalPath = this.CopyGrid();
-        this.Traverse(originalPath);
+        Dictionary<Location, char> originalPath = this.CopyGrid();
+        Dictionary<Location, HashSet<DirectionEnum>> traversalPath;
+        this.Traverse(originalPath, out traversalPath);
 
         // For each location along the path, place a new obstacle and check again.
-        List<Location> potentialObstacles = originalPath
-            .Where(kvp => kvp.Value.HasBeenTraversed())
+        List<Location> potentialObstacles = traversalPath
+            .Where(kvp => !kvp.Value.IsNullOrEmpty())
             .Select(kvp => kvp.Key)
             .ToList();
 
@@ -54,13 +54,13 @@ public class InputParser
             }
 
             // Make a fresh copy of the original grid.
-            Dictionary<Location, LocationMarker> grid = this.CopyGrid();
+            Dictionary<Location, char> grid = this.CopyGrid();
 
             // Place the obstacle.
-            grid[obstacle].Marker = '#';
+            grid[obstacle] = '#';
 
             // Run the path, and count this location if there's a loop.
-            loopLocations += this.Traverse(grid) ? 0 : 1;
+            loopLocations += this.Traverse(grid, out _) ? 0 : 1;
         }
 
         return loopLocations;
@@ -71,11 +71,14 @@ public class InputParser
     /// Returns an integer representing the number of potential traversal loops.
     /// </summary>
     /// <returns></returns>
-    private bool Traverse(Dictionary<Location, LocationMarker> grid)
+    private bool Traverse(Dictionary<Location, char> grid, out Dictionary<Location, HashSet<DirectionEnum>> traversalPath)
     {
+        // Keep track of where we've been and which direction we've traversed.
+        traversalPath = new Dictionary<Location, HashSet<DirectionEnum>>();
+
         // First, figure out where the guard is and which direction she's facing.
-        (Location location, LocationMarker startingMarker) = grid.First(kvp => "^>V<".Contains(kvp.Value.Marker));
-        DirectionEnum direction = DirectionEnumExtensions.GetDirectionForCharacter(startingMarker.Marker);
+        (Location location, char startingMarker) = grid.First(kvp => "^>V<".Contains(kvp.Value));
+        DirectionEnum direction = DirectionEnumExtensions.GetDirectionForCharacter(startingMarker);
         this.startingLocation = new Location(location);
 
         // Now figure out where the guard is going next.
@@ -85,25 +88,26 @@ public class InputParser
         while (grid.ContainsKey(nextStep))
         {
             // Cache some info.
-            LocationMarker locationMarker = grid[location];
-            LocationMarker nextStepMarker = grid[nextStep];
+            char locationMarker = grid[location];
+            char nextStepMarker = grid[nextStep];
 
-            // Determine if this is a potential loop.
-            if (locationMarker.HasBeenTraversedInDirection(direction))
+            // Determine if this is a potential loop
+            // by checking if we've already been in this space going in this direction.
+            if (traversalPath.GetValueOrDefault(location)?.Contains(direction) ?? false)
             {
                 // The guard is stuck in a loop, return false.
                 return false;
             }
 
             // Check if the next space forward is blocked.
-            if (nextStepMarker.Marker == '#')
+            if (nextStepMarker == '#')
             {
                 // Next space is blocked.
                 // Mark the current space as "traversed" in the original direction,
                 // then turn clockwise until we find a free space.
-                grid[location].MarkTraversed(direction);
+                traversalPath.AddToSetOrInsert(location, direction);
                 direction = direction.TurnClockwise();
-                grid[location].Marker = direction.GetCharacterForDirection();
+                grid[location] = direction.GetCharacterForDirection();
                 nextStep = location.GetLocationInDirection(direction, 1);
                 nextStepMarker = grid[nextStep];
                 continue;
@@ -111,9 +115,9 @@ public class InputParser
 
             // We are now facing an open direction.
             // Move forward one space and mark the location as traversed.
-            locationMarker.Marker = '.';
-            locationMarker.MarkTraversed(direction);
-            nextStepMarker.Marker = direction.GetCharacterForDirection();
+            locationMarker = '.';
+            traversalPath.AddToSetOrInsert(location, direction);
+            nextStepMarker = direction.GetCharacterForDirection();
 
             location = nextStep;
             nextStep = location.GetLocationInDirection(direction, 1);
@@ -122,8 +126,8 @@ public class InputParser
         // The guard is now leaving the map.
         // Change the character for her current position to an X,
         // and mark the location as traversed.
-        grid[location].Marker = '.';
-        grid[location].MarkTraversed(direction);
+        grid[location] = '.';
+        traversalPath.AddToSetOrInsert(location, direction);
 
         // The guard made it out of the map, return true.
         return true;
@@ -133,24 +137,24 @@ public class InputParser
     /// Copies the values of a grid into a new grid.
     /// </summary>
     /// <returns></returns>
-    private Dictionary<Location, LocationMarker> CopyGrid()
+    private Dictionary<Location, char> CopyGrid()
     {
-        Dictionary<Location, LocationMarker> gridCopy = new Dictionary<Location, LocationMarker>();
+        Dictionary<Location, char> gridCopy = new Dictionary<Location, char>();
 
-        foreach (KeyValuePair<Location, LocationMarker> kvp in this.grid)
+        foreach (KeyValuePair<Location, char> kvp in this.grid)
         {
-            Location location = kvp.Key;
-            LocationMarker marker = kvp.Value;
-
-            Location locationCopy = new Location(location);
-            LocationMarker markerCopy = new LocationMarker(marker);
-            gridCopy[locationCopy] = markerCopy;
+            Location locationCopy = new Location(kvp.Key);
+            gridCopy[locationCopy] = kvp.Value;
         }
-
-        return gridCopy;
+        return new Dictionary<Location, char>(this.grid);
     }
 
-    private string GetGridMap(Dictionary<Location, LocationMarker> grid)
+    /// <summary>
+    /// Small helper to print the grid in a user-friendly string.
+    /// </summary>
+    /// <param name="grid"></param>
+    /// <returns></returns>
+    private string GetGridMap(Dictionary<Location, char> grid)
     {
         int maxX = grid.Max(kvp => kvp.Key.X);
         int maxY = grid.Max(kvp => kvp.Key.Y);
@@ -160,7 +164,7 @@ public class InputParser
         {
             for (int x = 0; x < maxX; x++)
             {
-                gridMap += grid[new Location(x, y)].Marker;
+                gridMap += grid[new Location(x, y)];
             }
             gridMap += '\n';
         }
--- Shared/DictionaryExtensions.cs
+++ Shared/DictionaryExtensions.cs
@@ -4,7 +4,8 @@ namespace AdventOfCode2024.Shared;
 public static class DictionaryExtensions
 {
     /// <summary>
-    /// 
+    /// Helper to add an element to a list in a dictionary at the given key.
+    /// If the key is not in the dictionary yet, instantiates an empty list and inserts it.
     /// </summary>
     /// <typeparam name="TKey"></typeparam>
     /// <typeparam name="TValue"></typeparam>
@@ -15,6 +16,25 @@ public static class DictionaryExtensions
     {
         if (!dictionary.ContainsKey(key)) {
             dictionary[key] = new List<TValue>();
+        }
+
+        dictionary[key].Add(val);
+    }
+
+    /// <summary>
+    /// Helper to add an element to a set in a dictionary at the given key.
+    /// If the key is not in the dictionary yet, instantiates an empty set and inserts it.
+    /// </summary>
+    /// <typeparam name="TKey"></typeparam>
+    /// <typeparam name="TValue"></typeparam>
+    /// <param name="dictionary"></param>
+    /// <param name="key"></param>
+    /// <param name="val"></param>
+    public static void AddToSetOrInsert<TKey, TValue> (this Dictionary<TKey, HashSet<TValue>> dictionary, TKey key, TValue val) where TKey : notnull
+    {
+        if (!dictionary.ContainsKey(key))
+        {
+            dictionary[key] = new HashSet<TValue>();
         }
 
         dictionary[key].Add(val);
